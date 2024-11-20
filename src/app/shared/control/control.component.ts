892dcdbf0ce1763291a1db26693648aa6df4334c
Lucas Couto <loc.unb@gmail.com>
2024-11-20 16:28:38 -0300

    Using :host element to style to avoid unecessary wrapping

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -7,6 +7,10 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
+  /* Works only with ViewEncapsulation.None */
+  host: {
+    class: 'control',
+  },
 })
 export class ControlComponent {
   label = input.required<string>();
